5cabf4006f748b1fc192ccdf27e8e0bd62a33b24
Vasiliy Vanchuk <v.vanchuk@tut.by>
2021-08-10 17:42:31 +0300

    Use TS

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,13 @@ module.exports = {
     es2021: true,
     "jest/globals": true,
   },
-  extends: ["airbnb-base", "prettier"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "jest"],
+  extends: ["airbnb-base", "prettier", "plugin:@typescript-eslint/recommended"],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["jest"],
   rules: {
     "max-len": [
       "error",
@@ -18,5 +19,7 @@ module.exports = {
       },
     ],
     "import/prefer-default-export": "off",
+    "import/no-unresolved": "off", // https://github.com/typescript-eslint/typescript-eslint/issues/1624
+    "import/extensions": ["warn", "never"], // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/extensions.md
   },
 };
