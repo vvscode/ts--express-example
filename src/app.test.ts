a3df537250b305e0d56a7d8548f59b742ddc479f
Vasiliy Vanchuk <v.vanchuk@tut.by>
2021-08-10 21:07:28 +0300

    Session example test

--- src/app.test.ts
+++ src/app.test.ts
@@ -9,4 +9,32 @@ describe('app', () => {
       .expect(200, 'Hello World!')
       .end(done);
   });
+
+  it('calculates sum from  /personal_json', async () => {
+    const appRequest = request(app);
+    let cookies;
+
+    await appRequest.post('/personal_json')
+    .send({
+      value: 5
+    })
+    .expect(200, '{"count":5}')
+    .then((res) => {
+      cookies = res.headers["set-cookie"]
+    });
+
+    await appRequest.post('/personal_json')
+    .set('Cookie', cookies)
+    .send({
+      value: 5
+    })
+    .expect(200, '{"count":10}');
+
+    await appRequest.post('/personal_json')
+    .set('Cookie', cookies)
+    .send({
+      value: 6
+    })
+    .expect(200, '{"count":16}');
+  });
 });
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express from  'express';
 import {json}  from 'body-parser';
+import session from 'express-session';
+
+import './types'
 
 export const app = express();
 
 app.use(json({}));
+
+app.use(session({
+  secret:'Some secret here'
+}));
 
 let count = 0;
 
@@ -15,5 +22,16 @@ app.post('/json', (req, res) => {
   count += req.body.value;
   res.send({
     count
+  });
+});
+
+app.post('/personal_json', (req, res) => {
+  req.session.counter = req.session.counter || 0;
+  const {value} = req.body;
+  if (value) {
+    req.session.counter += value;
+  }
+  res.send({
+    count: req.session.counter
   });
 });
