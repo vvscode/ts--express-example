34d4eb19b9665d1d6d29842d7a331e5e80ebf6ff
Vasiliy Vanchuk <v.vanchuk@tut.by>
2021-08-10 20:42:51 +0300

    Save count on the app instance level

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
 import express from  'express';
+import {json}  from 'body-parser';
 
 export const app = express();
 
+app.use(json({}));
+
+let count = 0;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send(`Hello World!`)
+});
+
+app.post('/json', (req, res) => {
+  count += req.body.value;
+  res.send({
+    count
+  });
+});
