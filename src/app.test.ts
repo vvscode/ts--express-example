cb7f7e7fa28ae32b8984993408e55b8a2196b0fe
Vasiliy Vanchuk <v.vanchuk@tut.by>
2021-08-10 22:13:08 +0300

    Fix linting rules

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import request from 'supertest';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import MockDate from 'mockdate'
 
 
--- src/sum.ts
+++
@@ -1,4 +0,0 @@
-// sum function for numbers
-export function sum(a, b) {
-  return a + b;
-}
