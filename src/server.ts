d8be1c87538ed3eb8cf7763ed47db8a3a123216c
Vasiliy Vanchuk <v.vanchuk@tut.by>
2021-08-17 20:14:19 +0300

    Handle port number from env

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import './init';
 import { app } from './app';
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`App is listening at http://localhost:${port}`)
