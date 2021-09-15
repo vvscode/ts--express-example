3d3c2f91637f7975c2338b7d5289131e5c59c31a
Aleksandr Schemelev <prophreaker@gmail.com>
2021-09-15 22:42:10 +0300

    fix: change booksRouter's get request handler

--- src/controllers/booksRoute.ts
+++ src/controllers/booksRoute.ts
@@ -1,20 +1,25 @@
-import express from 'express';
-import Book from '../models/books.model';
+import express from "express";
+import Book from "../models/books.model";
 
 const booksRouter = express.Router();
 
-booksRouter.get("/", (req, res) =>  {
-  Book.find({}).then((response) => {console.log(response); res.end(); }); 
+booksRouter.get("/", (req, res) => {
+  Book.find({})
+    .then((response) => res.send(response))
+    .catch((e) => console.log(e));
 });
 
-booksRouter.post("/", (req, res) =>  {
+booksRouter.post("/", (req, res) => {
   const book = new Book({
     author: req.body.author,
     title: req.body.title,
-    year: req.body.year
+    year: req.body.year,
   });
 
-  book.save().then((response) => res.send(response));
+  book
+    .save()
+    .then((response) => res.send(response))
+    .catch((e) => console.log(e));
 });
 
 export default booksRouter;
